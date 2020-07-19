dc62909a55633eac1bbdcaa3a9cad634d4b4c5a2
ramingsky <maglasangramil@byui.edu>
2020-07-19 14:30:19 +0800

    y

--- project/FormToEmail.php
+++ project/FormToEmail.php
@@ -22,11 +22,11 @@ Enter the email address below to send the contents of the form to.  You can ente
 
 */
 
-$my_email = "mynetmaster2000@gmail.com";
-
+$my_email = "ramil.capangpangan@gmail.com";
 /*
 
 Optional.  Enter a From: email address.  Only do this if you know you need to.  By default, the email you get from the script will show the visitor's email address as the From: address.  In most cases this is desirable.  On the majority of setups this won't be a problem but a minority of hosts insist that the From: address must be from a domain on the server.  For example, if you have the domain example.com hosted on your server, then the From: email address must be something@example.com (See your host for confirmation).  This means that your visitor's email address will not show as the From: address, and if you hit "Reply" to the email from the script, you will not be replying to your visitor.  You can get around this by hard-coding a From: address into the script using the configuration option below.  Enabling this option means that the visitor's email address goes into a Reply-To: header, which means you can hit "Reply" to respond to the visitor in the conventional way.  (You can also use this option if your form does not collect an email address from the visitor, such as a survey, for example, and a From: address is required by your email server.)  The default value is: $from_email = "";  Enter the desired email address between the quotes, like this example: $from_email = "contact@example.com";  In these cases, it is not uncommon for the From: ($from_email) address to be the same as the To: ($my_email) address, which on the face of it appears somewhat goofy, but that's what some hosts require.
+mynetmaster2000@gmail.com";
 
 */
 
